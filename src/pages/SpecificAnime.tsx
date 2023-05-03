2a8806a624a8252cbf72f8cea1ac14172cf6a48a
AndrewAC21 <88219834+AndrewAC21@users.noreply.github.com>
2023-05-03 00:35:05 +0000

    style specific anime page

--- src/pages/SpecificAnime.tsx
+++ src/pages/SpecificAnime.tsx
@@ -1,6 +1,37 @@
 import { useContext } from "react";
+import styled from "styled-components";
 import { AnimesContext } from "../context/AnimesContext";
 import { Anime, AnimeContextType } from "../types";
+
+const ContainerAnime = styled.div`
+  width: 90%;
+  height: 80vhs;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+
+  > h1 {
+    font-size: 2rem;
+    margin: 1rem;
+    text-transform: uppercase;
+    align-self: center;
+    text-align: center;
+  }
+`;
+const Image = styled.img`
+  width: 100%;
+  max-width: 350px;
+  margin: 1rem auto;
+  border-radius: 10px;
+`;
+
+const Description = styled.p`
+  width: 90%;
+  font-size: 1.2rem;
+  text-align: justify;
+  margin: 1rem auto;
+`;
 interface Props {
   params: {
     id: number;
@@ -12,14 +43,12 @@ function SpecificAnime({ params }: Props) {
   let selectedAnime: Anime | undefined = globalAnimes.find(
     (singleAnime) => singleAnime.id == params.id
   );
-  console.log(selectedAnime);
   return (
-    <>
+    <ContainerAnime>
       <h1>{selectedAnime?.title}</h1>
-      <img src={selectedAnime?.image} alt="" />
-      <p>{selectedAnime?.description}</p>
-
-    </>
+      <Image src={selectedAnime?.image} alt="" />
+      <Description>{selectedAnime?.description}</Description>
+    </ContainerAnime>
   );
 }
 
